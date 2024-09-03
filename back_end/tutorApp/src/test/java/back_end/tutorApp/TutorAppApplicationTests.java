566c293352ce5635ff827413149658d9f9f36e35
GaryMcflarry <122262751+GaryMcflarry@users.noreply.github.com>
2024-09-03 16:03:17 +0200

    back_end setup commit

---
+++ back_end/tutorApp/src/test/java/back_end/tutorApp/TutorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package back_end.tutorApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
