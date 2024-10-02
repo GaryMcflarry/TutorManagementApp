b431b2f3d92c46c4e678983db825b607992dc7db
GaryMcflarry <122262751+GaryMcflarry@users.noreply.github.com>
2024-10-02 14:45:41 +0200

    Custom Button Commit

--- Front_End/LearnTutor/app/components/CustomButton.jsx
+++ Front_End/LearnTutor/app/components/CustomButton.jsx
@@ -1,14 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { TouchableOpacity, Text } from 'react-native'
 import React from 'react'
+import { StyleSheet } from "react-native"
 
-const customButton = () => {
+//creating custom function to be use accross all pages (component)
+//params
+const CustomButton = ({title, handlePress, containerStyles, isLoading}) => {
+  //component structure
   return (
-    <View>
-      <Text>customButton</Text>
-    </View>
+    <TouchableOpacity 
+    //press functionality 
+    onPress={handlePress}
+    activeOpacity={0.7}
+    className={`bg-tertiary justify-center items-center	${containerStyles} `}
+    style={styles.shadow}
+    //good for disabling the button while the app is still loading
+    disabled={isLoading}>
+      <Text className={`text-white font-psemibold text-lg`}>{title}</Text>
+    </TouchableOpacity>
   )
 }
 
-export default customButton
+//nativewind Shadow doesn't work for android devices on nativewind.
+const styles = StyleSheet.create({
+  shadow: {
+      elevation: 20,
+  }
+})
 
-const styles = StyleSheet.create({})
+//allowing it to be used
+export default CustomButton
