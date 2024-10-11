222eb401a764fd669e00d518ab94a51102de6872
GaryMcflarry <122262751+GaryMcflarry@users.noreply.github.com>
2024-10-11 12:49:54 +0200

    Status bar commit

--- Front_End/LearnTutor/app/(pages)/home.jsx
+++ Front_End/LearnTutor/app/(pages)/home.jsx
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from 'react'
+import { StatusBar } from "expo-status-bar";
+
 
 
 const home = () => {
@@ -12,10 +14,11 @@ const home = () => {
       
       <View className="bg-primary h-20 justify-center items-center" style={styles.shadow}>
         <Text className="text-white font-pregular text-xs" style={styles.shadow}>14:43</Text>
-        <Text className="text-white font-pregular text-2xl">Account</Text>
+        <Text className="text-white font-pregular text-3xl">Account</Text>
       </View>
       
     </ScrollView>
+    <StatusBar backgroundColor="#FEA07D" style="light" />
     </SafeAreaView>
   )
 }
