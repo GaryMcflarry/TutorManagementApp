a94eacd31a90123077122faac3c8e9231c1bdfe5
10die <simtendai@gmail.com>
2024-10-12 01:15:21 +0200

    created the status bar component with a children component

--- Front_End/LearnTutor/app/(pages)/home.jsx
+++ Front_End/LearnTutor/app/(pages)/home.jsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from 'react'
 import { StatusBar } from "expo-status-bar";
+import StatusBarWrapper from '../components/statusBar';
 
 
 
@@ -10,15 +11,11 @@ const home = () => {
 
   return (
     <View className="bg-white h-full w-full">
-    <ScrollView contentContainerStyle={{ height: "100%" }}>
-      
-      <View className="bg-primary h-20 justify-center items-center mt-5" style={styles.shadow}>
-        {/* <Text className="text-white font-pregular text-xs" style={styles.shadow}>14:43</Text> */}
-        <Text className="text-white font-pregular text-xl">Account</Text>
-      </View>
-      
-    </ScrollView>
-    <StatusBar backgroundColor="#FEA07D" style="light" />
+    <StatusBarWrapper>
+      <Text>
+        Home
+      </Text>
+    </StatusBarWrapper>
     </View>
   )
 }
