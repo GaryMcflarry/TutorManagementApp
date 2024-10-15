8f785e535e5c059fac220f8f18dd58a866c4a082
SetrenNettamyn <cb.amalgam@gmail.com>
2024-10-15 14:48:58 +0200

    to view changes real time

--- Front_End/LearnTutor/app/(auth)/sign-in.jsx
+++ Front_End/LearnTutor/app/(auth)/sign-in.jsx
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from "react-native-safe-area-context";
+import ResourceCard from '../components/ResourceCard';
 const signIn = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
     <ScrollView contentContainerStyle={{ height: "100%" }}>
       <Text className="text-tertiary font-pregular text-2xl">SignUp</Text>
+      <ResourceCard>test</ResourceCard>
     </ScrollView>
     </SafeAreaView>
   )
