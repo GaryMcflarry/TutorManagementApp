0559822eaade878430bd8cb1e9faaab6b0e64bae
GaryMcflarry <122262751+GaryMcflarry@users.noreply.github.com>
2024-10-18 14:44:17 +0200

    menu button edited, and icon edits

--- Front_End/LearnTutor/app/(pages)/home.jsx
+++ Front_End/LearnTutor/app/(pages)/home.jsx
@@ -1,41 +1,37 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import React from 'react'
+import React from "react";
 import { StatusBar } from "expo-status-bar";
-import StatusBarWrapper from '../components/statusBar';
-import CustomButton from '../components/CustomButton';
+import StatusBarWrapper from "../components/statusBar";
+import CustomButton from "../components/CustomButton";
 import { Redirect, router } from "expo-router";
+import MenuButton from "../components/MenuButton";
 
-
-
-
-
-const Home = ({navigation}) => {
-
-
+const Home = ({ navigation }) => {
   return (
     <View className="bg-white h-full w-full">
-    <StatusBarWrapper title="Home">
-      <Text>
-        Home
-      </Text>
-         <CustomButton
-              title="Menu But"
-              handlePress={() => navigation.toggleDrawer()}
-              containerStyles="w-[110px] h-[62px] justify-center items-center"
-            />
-    </StatusBarWrapper>
+      <StatusBarWrapper title="Home">
+        <View className="w-full h-[70px] items-end p-3">
+          <MenuButton
+           handlePress={() => navigation.toggleDrawer()} 
+            
+           />
+        </View>
+        <Text>Home</Text>
+        <CustomButton
+          title="Menu But"
+          handlePress={() => navigation.toggleDrawer()}
+          containerStyles="w-[110px] h-[62px] justify-center items-center"
+        />
+      </StatusBarWrapper>
     </View>
-  )
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
   shadow: {
-      elevation: 50,
-  }
-})
+    elevation: 50,
+  },
+});
 
-export default Home
-
-
+export default Home;
